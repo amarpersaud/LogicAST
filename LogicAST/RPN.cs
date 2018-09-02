b38ad62b127d6030611762e1f668373de277451c
Amar Persaud <tehspartaa@gmail.com>
2018-09-02 00:38:58 -0400

    Added RPN class and StringExtensions Add RPN class for parsing infix into RPN, and later from RPN into IExpression for use in arguments. StringExtensions for removing whitespace.

---
+++ LogicAST/RPN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicAST
+{
+    public class RPN
+    {
+
+        /// <summary>
+        /// Formats an infix string with proper whitespace
+        /// </summary>
+        /// <param name="infix"></param>
+        /// <returns></returns>
+        public string FormatInfixString(string infix)
+        {
+            infix = infix.TrimAllWhitespace();
+
+            //TODO: implement
+
+            return infix;
+        }
+
+        public string InfixToRPN(string infixString)
+        {
+            Stack<string> output = new Stack<string>();
+            Stack<string> operatorStack = new Stack<string>();
+
+            //TODO: implement
+
+            //Concatenate into a single string with space as delimeter
+            return output.Aggregate((x,c) => x + " " +c);
+        }
+
+    }
+}
---
+++ LogicAST/StringExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LogicAST
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceAll(this string str, string[] OldString, string NewString)
+        {
+            if(str is null)
+            {
+                return null;
+            }
+            foreach(string s in OldString)
+            {
+                str = str.Replace(s, NewString);
+            }
+            return str;
+        }
+
+        public static string RemoveWhitespace(this string str)
+        {
+            string output = "";
+
+            foreach(char c in str)
+            {
+                if (!string.IsNullOrWhiteSpace(new string(c, 1)))
+                {
+                    output += c;
+                }
+            }
+
+            return output;
+        }
+
+        // from https://www.codeproject.com/Articles/1014073/Fastest-method-to-remove-all-whitespace-from-Strin
+        static Regex whitespace = new Regex(@"\s+", RegexOptions.Compiled);
+
+        public static string TrimAllWhitespace(this string str)
+        {
+            return whitespace.Replace(str, "");
+        }
+
+    }
+}
