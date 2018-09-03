88f21824db897542d63201acc1c8207252fab51e
Amar Persaud <tehspartaa@gmail.com>
2018-09-02 20:55:41 -0400

    Added Unit Test project with RPN Tests

---
+++ LogicAST.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicAST.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicAST.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c9dcaeb7-2690-469d-b9d1-b58f4b18c3f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LogicAST.Test/RPNTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LogicAST;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicAST.Tests
+{
+    [TestClass()]
+    public class RPNTests
+    {
+        [TestMethod()]
+        public void InfixToRPNTest()
+        {
+            string res = RPN.InfixToRPN("pv~Q");
+
+
+            Assert.AreEqual("p Q ~ v", res);
+        }
+
+        [TestMethod()]
+        public void FormatInfixStringTest()
+        {
+            string[] props = {
+                "a&b>c",
+                "c^d=f",
+                "PropA &PropB^PropC>PropD",
+                "pv~Q"
+            };
+
+            string[] results =
+            {
+                "a & b > c",
+                "c ^ d = f",
+                "PropA & PropB ^ PropC > PropD",
+                "p v ~ Q"
+            };
+            for(int i = 0; i < props.Length; i++)
+            {
+                Assert.AreEqual(RPN.FormatInfixString(props[i]), results[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void IsOperatorTest()
+        {
+            string[] operators = { "=>", "->", "!", "~", "=", "&" };
+            string[] nonOperators = { "#", "@", "%", "*", "a", "sin", "cos"};
+
+            foreach(string s in operators)
+            {
+                Assert.IsTrue(LogicAST.LegalCharacters.IsOperator(s));
+            }
+            foreach(string s in nonOperators)
+            {
+                Assert.IsFalse(LogicAST.LegalCharacters.IsOperator(s));
+            }
+        }
+    }
+}
