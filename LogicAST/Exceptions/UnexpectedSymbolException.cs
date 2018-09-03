ec071377a426f6b53a3ef15337915e97366774ab
Amar Persaud <tehspartaa@gmail.com>
2018-09-02 20:42:07 -0400

    Moved UnexpectedSymbolException to a new file

---
+++ LogicAST/Exceptions/UnexpectedSymbolException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicAST.Exceptions
+{
+    [System.Serializable]
+    public class UnexpectedSymbolException : Exception
+    {
+        public UnexpectedSymbolException() { }
+        public UnexpectedSymbolException(string message) : base(message) { }
+        public UnexpectedSymbolException(string message, Exception inner) : base(message, inner) { }
+        protected UnexpectedSymbolException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- LogicAST/Expressions/IExpression.cs
+++ LogicAST/Expressions/IExpression.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LogicAST.Exceptions;
 
 namespace LogicAST.Expressions
 {
@@ -10,16 +11,5 @@ namespace LogicAST.Expressions
         bool Evaluate();
         void Set(char Letter, bool Value);
         string ToString();
-    }
-
-    [System.Serializable]
-    public class UnexpectedSymbolException : Exception
-    {
-        public UnexpectedSymbolException() { }
-        public UnexpectedSymbolException(string message) : base(message) { }
-        public UnexpectedSymbolException(string message, Exception inner) : base(message, inner) { }
-        protected UnexpectedSymbolException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
