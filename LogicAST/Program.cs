a4a63a7071bfadeead0d6dad15eb9af326da60a0
Amar Persaud <tehspartaa@gmail.com>
2018-09-06 20:46:36 -0400

    Renaming UI project

--- LogicAST/Program.cs
+++ LogicAST/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LogicAST.Expressions;
+using Logic.Base;
 
-namespace LogicAST
+namespace Logic.UI
 {
     class Program
     {
